190c1d719214bb2e3afe743d3ed8aab0f52b8ad0
Ben Shyong <likeachild@gmail.com>
2013-05-13 13:17:39 -0400

    gallery skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yolocn::Application.config.secret_token = 'e09582fc19313cd42da070fdcfd2d57072f65d6e5f3cd75332537d5dc6307796b99df94bb2535f192684ffb55c450880d68fd26b1e670532187eb8f8d86a812c'
