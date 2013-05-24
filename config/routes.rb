dbb6a5306d6db12501f22290ca6c47a3e494b339
Ben Shyong <likeachild@gmail.com>
2013-05-24 12:42:01 -0400

    products scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Yolocn::Application.routes.draw do
 
+  resources :products
+
+
   devise_for :users
 
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130523214951) do
+ActiveRecord::Schema.define(:version => 20130524164031) do
+
+  create_table "products", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.text     "picture_urls"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
   create_table "rails_admin_histories", :force => true do |t|
     t.text     "message"
