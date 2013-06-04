35151d8feb2cc854219d0c72ecb43578ba941183
Ben Shyong <likeachild@gmail.com>
2013-06-04 11:11:34 +0800

    prelim picture urls

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,10 @@ class Product < ActiveRecord::Base
 
   monetize :price_dollars, as: :price
 
-  attr_accessible :description, :name, :picture_urls
+  attr_accessible :description, :name, :picture_urls, :price_dollars
+
+  def picture_urls
+    super.split(',')
+  end
 
 end
